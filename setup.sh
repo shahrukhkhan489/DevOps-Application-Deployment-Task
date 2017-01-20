187a6ff2617d0077c9d4f2dc3fa8b1323e9d8a05
Shahrukh Khan <shahrukhkhan489@gmail.com>
2017-01-20 13:02:12 +0530

    Update

--- setup.sh
+++ setup.sh
@@ -4,8 +4,6 @@ wget  https://releases.hashicorp.com/vagrant/1.9.1/vagrant_1.9.1_x86_64.rpm
 rpm -ivh vagrant_1.9.1_x86_64.rpm
 sudo yum install -y epel-release ansible
 
-#vagrant up
-
 
 wget -O /etc/yum.repos.d/virtualbox.repo http://download.virtualbox.org/virtualbox/rpm/rhel/virtualbox.repo
 yum -y update
@@ -18,3 +16,4 @@ yum install -y binutils gcc make patch libgomp glibc-headers glibc-devel kernel-
 service vboxdrv setup
 usermod -a -G vboxusers root
 
+vagrant up
