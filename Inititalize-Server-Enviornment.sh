ff67b078956e51a524d5c31d796b10775b20476c
Shahrukh Khan <shahrukhkhan489@gmail.com>
2017-01-20 15:25:58 +0530

    Update

---
+++ Inititalize-Server-Enviornment.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+wget  https://releases.hashicorp.com/vagrant/1.9.1/vagrant_1.9.1_x86_64.rpm
+rpm -ivh vagrant_1.9.1_x86_64.rpm
+sudo yum install -y epel-release ansible
+
+
+wget -O /etc/yum.repos.d/virtualbox.repo http://download.virtualbox.org/virtualbox/rpm/rhel/virtualbox.repo
+yum -y update
+
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+yum install -y binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-devel kernel-PAE-devel dkms VirtualBox-5.1
+
+/usr/lib/virtualbox/vboxdrv.sh setup
+
+service vboxdrv setup
+usermod -a -G vboxusers root
+
+vagrant up
