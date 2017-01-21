491910dbd235f8ab4b3a60ae01d0b2549952a0de
Shahrukh Khan <shahrukhkhan489@gmail.com>
2017-01-21 13:59:15 +0530

    update

--- Inititalize-Vagrant-Server-Enviornment.sh
+++ Inititalize-Vagrant-Server-Enviornment.sh
@@ -8,6 +8,9 @@ sudo yum install -y epel-release ansible
 wget -O /etc/yum.repos.d/virtualbox.repo http://download.virtualbox.org/virtualbox/rpm/rhel/virtualbox.repo
 yum -y update
 
+yum remove -y kernel-devel
+yum install -y kernel-devel
+
 rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 yum install -y binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-devel kernel-PAE-devel dkms VirtualBox-5.1
 
@@ -16,4 +19,24 @@ yum install -y binutils gcc make patch libgomp glibc-headers glibc-devel kernel-
 service vboxdrv setup
 usermod -a -G vboxusers root
 
+mkdir box; cd box;
+
+cat > Vagrantfile << EOF
+Vagrant.configure("2") do |config|
+
+	config.vm.box = "hashicorp/precise64"
+
+
+	config.vm.provider "virtualbox" do |vb|
+		vb.memory = "2048"
+	end
+
+	config.vm.provision "ansible" do |ansible|
+		ansible.playbook = "playbook.yml"
+	end
+
+end
+EOF
+
 vagrant up
+
--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,6 @@
 Vagrant.configure("2") do |config|
 
-	config.vm.box = "hashicorp/precise64"
+	config.vm.box = "ubuntu/trusty64"
 
 
 	config.vm.provider "virtualbox" do |vb|
